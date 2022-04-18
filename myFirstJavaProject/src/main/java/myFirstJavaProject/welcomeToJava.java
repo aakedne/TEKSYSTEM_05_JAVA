834515aabd66f993e2213b42cb1e0aac00edf160
Abdikadir <54958020+aakedne@users.noreply.github.com>
2022-04-18 14:28:11 -0500

    message

---
+++ myFirstJavaProject/src/main/java/myFirstJavaProject/welcomeToJava.java
@@ -0,0 +1,37 @@
+package myFirstJavaProject;
+import java.util.Scanner;
+
+public class welcomeToJava {
+	
+public static void main(String [] ars) {
+		//System.out.println("Welcome to Java!");
+		
+		//double radius;
+		//double area;
+		
+		// assign a radius
+		//radius = 20;
+		
+		// compute area
+		//area = radius * radius * 3.24159;
+		
+		//Display results
+		
+		//System.out.println("The result" + " is "+ area);
+		
+		
+		// Creating and closing scanner inputs
+		//Scanner userInput = new Scanner(System.in);
+		//System.out.println("Enter your name :");
+		//String firstName = userInput.nextLine();
+		
+		//System.out.println("My name is " + firstName);
+		
+		//userInput.close();
+
+		
+		int = 20;
+		
+	}}
+
+}
